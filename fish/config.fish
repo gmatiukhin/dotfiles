048418e8f70f325ad586afd64aa764af0de55f55
Grigorii Matiukhin <grigory36m@gmail.com>
2023-06-24 13:53:31 +0300

    added go to PATH, better kb layout switching

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
   fish_add_path $HOME/.cargo/bin # cargo
   fish_add_path $HOME/.local/bin # pip
+  fish_add_path $HOME/go/bin # golang
   fish_add_path /usr/lib/node_modules # npm
 
   abbr --add grep rg
