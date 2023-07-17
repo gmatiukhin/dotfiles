4cf18f1ec85d170c3704db5089ad1dd08b896104
Grigorii Matiukhin <grigory36m@gmail.com>
2023-07-17 16:55:09 +0300

    improved lock script

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
+amixer set Master mute # turn off volume
 xkb-switch -s us # switch to latin keyboard before locking
 convert $HOME/.config/i3/background/cory-billett-luthadel-bt2.jpg RGB:- \
   | i3lock --nofork --ignore-empty-password --raw 1920x1080:rgb --image /dev/stdin
