dfffa1c250b54ed08ecbf3e0a65fbe28a6c6d441
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-05-09 17:30:58 +0300

    optionally specify python version

--- fish/functions/venv.fish
+++ fish/functions/venv.fish
@@ -1,11 +1,15 @@
 # creates and activates python's venv in a directory where there's no venv
 # alternatively deactivates it
-function venv
+function venv -a python
   if test -e "$VIRTUAL_ENV"
     deactivate
   else
     if not test -e .venv
-      python3 -m venv .venv
+      if string match -req "python3\.\d+" $python
+        $python -m venv .venv
+      else
+        python3 -m venv .venv
+      end
     end
     source .venv/bin/activate.fish
   end
