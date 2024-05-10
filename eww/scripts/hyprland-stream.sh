4e97cf44d6a77aee3f08f2a404f1504ec33b518b
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-05-11 00:07:13 +0300

    minor changes and fixes

--- eww/scripts/hyprland-stream.sh
+++ eww/scripts/hyprland-stream.sh
@@ -40,5 +40,5 @@ function handle {
   esac
 }
 
-socat - "UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" \
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" \
   | while read -r line; do handle "$line"; done
