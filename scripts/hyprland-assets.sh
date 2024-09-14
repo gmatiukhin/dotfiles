52212b56dd84ac65b88c24f1487907efc1602f5f
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-09-14 20:36:39 +0300

    hyprland fixes

---
+++ scripts/hyprland-assets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# temorarily fixes Hyprland's assets on Arch
+# https://github.com/hyprwm/Hyprland/issues/7319#issuecomment-2288938303
+ln -s /usr/share/hyprland/ /usr/local/share/hypr
