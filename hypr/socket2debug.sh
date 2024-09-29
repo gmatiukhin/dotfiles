d6e3d9899b87078a1d9a82803d22aec53f4aa278
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-09-29 18:01:32 +0300

    change unix socket path in debug script

--- hypr/socket2debug.sh
+++ hypr/socket2debug.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-socat - "UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" \
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" \
   | while read -r line; do echo $line; done
