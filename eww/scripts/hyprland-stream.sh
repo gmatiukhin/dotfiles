9342cb869b3b877b0aa302c17f6488cbe7993a5b
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-10-08 17:43:25 +0300

    stop creating useless file

--- eww/scripts/hyprland-stream.sh
+++ eww/scripts/hyprland-stream.sh
@@ -51,7 +51,5 @@ function handle {
   esac
 }
 
-touch ~/hyprstream.log
-
 socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" |
   while read -r line; do handle "$line"; done
